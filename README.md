388cb1907fc21ca7287aa99510ba6e5d116bf5ca
Alexander Koritskiy <alexkoritsa@gmail.com>
2023-05-18 22:04:10 +0300

    Load project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Teamsync
+Проект разделен на две части - бекенд и фронтенд.
+
+## Backend
+Технологический стек:
+1. Java
+2. SpringBoot
+3. Maven
+
+## Frontend
+Технологический стек:
+1. VueJs
+2. TailwindCSS
---
+++ backend/hiring/src/main/java/com/koritski/teamsync/hiring/constants/AuthConstants.java
@@ -0,0 +1,10 @@
+package com.koritski.teamsync.hiring.constants;
+
+public final class AuthConstants {
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String PERMISSION_DESCRIPTION
+            = "You don't have permission to do this. Please, try again later.";
+
+    private AuthConstants() {
+    }
+}
